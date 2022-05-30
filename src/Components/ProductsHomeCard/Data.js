376248eba672e9c4117f7a063e2b2cd5d464d20c
jpaniag2 <jpaniag2@gmail.com>
2022-05-29 22:57:27 -0400

    Created card to display azteca bakery products.

---
+++ src/Components/ProductsHomeCard/Data.js
@@ -0,0 +1,31 @@
+export const BreadCard = {
+    alt: 'bread',
+    title: 'Teleras & Bolillos', 
+    linkRoute: '/menu',
+    img: require('../../images/ProductCardPictures/panDulceCard.JPG')
+
+}
+
+export const PanDulceCard = {
+    alt: 'pandulce',
+    title: 'Pan Dulce', 
+    linkRoute: '/menu',
+    img: require('../../images/ProductCardPictures/panDulceCard.JPG')
+ 
+}
+
+export const PastriesCard = {
+    alt: 'pastries',
+    title: 'Pastries', 
+    linkRoute: '/menu',
+    img: require('../../images/ProductCardPictures/pastriesCard2.JPG')
+ 
+}
+
+export const SeasonalCard = {
+    alt: 'seasonal',
+    title: 'Seasonal', 
+    linkRoute: '/menu',
+    img: require('../../images/ProductCardPictures/RoscaDeReyes.jpeg')
+ 
+}
---
+++ src/Components/ProductsHomeCard/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Card, Button, Nav, Container } from 'react-bootstrap'
+import styled from 'styled-components'
+
+const Styles = styled.div`
+
+    .nav-link{
+        border:1px solid transparent;
+        transition: 0.5s ease-out;
+        opacity: 1;
+        backgroundColor: #000;
+        max-width: 100%;
+
+        &:hover{
+            cursor: inherit;
+            transform: translateY(-10px);
+            transition:  0.5s ease-out;
+        }
+    }
+
+    .card-img-top{
+        height: 320px ;
+        width: 100% ;
+        object-fit: cover !important;
+        position: center;
+
+        @media screen and (max-width: 480px){
+        height: 200px;
+    }
+    }
+
+`
+
+const ProductsHomeCard = ({ title, linkRoute, img }) => {
+    return (
+
+        <Card style={{ backgroundColor: 'transparent', border: 'none' }}>
+            <Styles>
+                <Nav.Link href="">
+                    <Card.Img variant="top" src={img} />
+                </Nav.Link>
+            </Styles>
+
+
+
+            <Card.Body >
+                <Card.Title style={{fontFamily: 'Khula', fontSize: '1.3rem', fontWeight: '600'}}>{title}</Card.Title>
+            </Card.Body>
+            {/* <Card.Footer>
+                    <Button variant="warning" href={linkRoute}>Warning</Button>
+                </Card.Footer> */}
+        </Card>
+
+    )
+}
+
+export default ProductsHomeCard
