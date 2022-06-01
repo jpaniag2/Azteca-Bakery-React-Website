2e20e5dfacc17eec924b990761ed8959b57b19ae
jpaniag2 <jpaniag2@gmail.com>
2022-06-01 01:00:06 -0400

    Updated footer logo and brand name.

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import { animateScroll as scroll } from 'react-scroll/modules';
-import { FooterContainer, FooterWrap, 
-    FooterLinksContainer, FooterLinksWrapper, 
-    FooterLinkItems, FooterLinkTitle, 
-    FooterLink, SocialMedia, SocialMediaWrap, 
-    SocialLogo, SocialLogo2, WebsiteRights, SocialIcons, 
-    SocialIconLink } from './FooterStyle';
+import Logo from '../../images/DarkGreenLogo.png'
+import {
+    FooterContainer, FooterWrap,
+    FooterLinksContainer, FooterLinksWrapper,
+    FooterLinkItems, FooterLinkTitle,
+    FooterLink, SocialMedia, SocialMediaWrap,
+    SocialLogo, SocialLogo2, WebsiteRights, SocialIcons,
+    SocialIconLink
+} from './FooterStyle';
 
 const Footer = () => {
 
@@ -15,41 +19,27 @@ const Footer = () => {
         scroll.scrollToTop();
     }
 
-  return (
-      
-    <FooterContainer>
-    <FooterWrap>
-        <FooterLinksContainer>
-            <FooterLinksWrapper>
+    return (
 
-            </FooterLinksWrapper>
-        </FooterLinksContainer>
+        <Container fluid style={{ backgroundColor: '#fff' }}>
+            <FooterWrap>
+            <img
+                            src={Logo}
+                            width="100"
+                            height="100"
+                            className="d-inline-block"
+                            alt="logo"
+                            style={{margin: '4rem 0'}}
 
-        <SocialMedia>
-            <SocialMediaWrap>
-            <SocialIcons>
-{/* 
-<SocialIconLink href="/" target="_blank" aria-label="Facebook">
-    <FaFacebook />
-</SocialIconLink>
-
-<SocialIconLink href="/" target="_blank" aria-label="Instagram">
-    <FaInstagram />
-</SocialIconLink> */}
-
-</SocialIcons>
+                        />
                 <SocialLogo to="/" onClick={toggleHome}>
-                    azteca bakery © {new Date().getFullYear()}
+                    Azteca Bakery © {new Date().getFullYear()}
                 </SocialLogo>
-                <WebsiteRights> Made by CrunchyVision©</WebsiteRights>
+                <WebsiteRights> Made by Julio Paniagua©</WebsiteRights>
 
-            </SocialMediaWrap>
-            
-        </SocialMedia>
-
-    </FooterWrap>
-</FooterContainer>
-  )
+            </FooterWrap>
+        </Container>
+    )
 }
 
 export default Footer;
